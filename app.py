68a042f3791fdb83ca9354178687b94abd822b77
helldance <helldancing@gmail.com>
2022-03-24 07:56:15 +0800

    checkpoint

--- app.py
+++ app.py
@@ -11,13 +11,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class AlcDateTime(db.TypeDecorator):
     impl = db.DateTime
-    
+
     def process_bind_param(self, value, dialect):
         if type(value) is str:
             return datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
         return value
+
 
 class Tweet(db.Model):
     author_id = db.Column(db.String(512))
@@ -27,13 +29,15 @@ class Tweet(db.Model):
     subjectivity = db.Column(db.Float())
     tone = db.Column(db.String(8))
 
+
 @app.route("/")
 def home():
     # past 1 hour tweets
-    past_hour = datetime.now() - timedelta(hours= 2)
+    past_hour = datetime.now() - timedelta(hours=3)
     #print("past hour {}".format(past_hour))
     tweets = Tweet.query.filter(Tweet.date >= past_hour).all()
     return render_template("base.html", tweetList=tweets)
+
 
 @app.route("/ping", methods=['POST', 'GET'])
 def listen_agg_stat():
@@ -46,7 +50,7 @@ def listen_agg_stat():
     db.session.commit()
 
     return redirect(url_for("home"))
-    
+
 
 if __name__ == "__main__":
     db.create_all()
